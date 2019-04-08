22c7cf3390ebb9eb80b9fc20e982faa76b0c819f
Alexander Sharp <s18107249@bcud2-010048.students.uce.ac.uk>
2019-04-08 12:40:45 +0100

    first commit

---
+++ ASDproject/ASDproject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ASDproject
+//
+//  Created by Alexander Sharp on 08/04/2019.
+//  Copyright © 2019 Alexander Sharp. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
